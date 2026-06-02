8e74280bd6e3fac6fe1ca29016615208603fb1d8
Youness Zbair <38602794+youness52@users.noreply.github.com>
2026-06-02 03:21:13 +0100

    Update initial URL in UploadProjects.js

--- screens/UploadProjects.js
+++ screens/UploadProjects.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, Button, BackHandler, Linking, StyleSheet, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-const initialUrl = 'https://miage.page.gd/';
+const initialUrl = 'https://zbair.net/miage.html';
 
 export default function UploadProjects() {
   const webViewRef = useRef(null);
